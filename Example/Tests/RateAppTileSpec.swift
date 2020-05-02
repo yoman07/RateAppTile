5b7aaa32e935414e5e5c55affabaaaea285d121f
Roman Barzyczak <roman.barzyczak@gmail.com>
2020-05-02 14:58:10 +0200

    first commit

---
+++ Example/Tests/RateAppTileSpec.swift
@@ -0,0 +1,9 @@
+//
+//  RateAppTileSpec.swift
+//  RateAppTile_Example
+//
+//  Created by Roman Barzyczak on 02/05/2020.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import Foundation
