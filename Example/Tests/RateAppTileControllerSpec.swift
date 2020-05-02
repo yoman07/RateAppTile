5df6f98eba3d9c1285d70d1eb23a2e0d642a545e
Roman Barzyczak <roman.barzyczak@gmail.com>
2020-05-02 19:09:03 +0200

    Fixes for showing rate

---
+++ Example/Tests/RateAppTileControllerSpec.swift
@@ -0,0 +1,9 @@
+//
+//  RateAppTileControllerSpec.swift
+//  RateAppTile_Example
+//
+//  Created by Roman Barzyczak on 02/05/2020.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import Foundation
