7d70f807430c0a2450c91119b829d7e1aa4957fd
Roman Barzyczak <roman.barzyczak+web@gmail.com>
2020-05-04 14:36:24 +0200

    Fix for choosing when to show rate

--- Example/Tests/RateAppTileControllerSpec.swift
+++ Example/Tests/RateAppTileControllerSpec.swift
@@ -1,9 +1,83 @@
-//
-//  RateAppTileControllerSpec.swift
-//  RateAppTile_Example
-//
-//  Created by Roman Barzyczak on 02/05/2020.
-//  Copyright © 2020 CocoaPods. All rights reserved.
-//
+import Quick
+import Nimble
+import UIKit
+import RateAppTile
 
-import Foundation
+class RateAppTileControllerSpec: QuickSpec {
+    override func spec() {
+        describe("rate app tile controlle") {
+  
+            it("should return tile") {
+                let userDefaults: UserDefaults = UserDefaults(suiteName: "rateAppTestTile")!
+                let controller: RateAppTileController = RateAppTileController(userDefaults: userDefaults)
+                
+                let rateAppConfig =  RateAppConfig()
+                rateAppConfig.numberOfLaunches = 20
+                userDefaults.set(false, forKey: rateAppConfig.rateAppTileDisplayed)
+                userDefaults.set(25, forKey: rateAppConfig.numberOfLaunchesDefaultsKey)
+                
+                controller.rateAppConfig = rateAppConfig
+                
+
+                expect(controller.nextAction) == .showRateTile
+            }
+            
+            it("should return nothing because has been displayed") {
+                let userDefaults: UserDefaults = UserDefaults(suiteName: "rateAppTestTileTrue")!
+                let controller: RateAppTileController = RateAppTileController(userDefaults: userDefaults)
+                
+                let rateAppConfig =  RateAppConfig()
+                rateAppConfig.numberOfLaunches = 20
+                
+                userDefaults.set(25, forKey: rateAppConfig.numberOfLaunchesDefaultsKey)
+                userDefaults.set(true, forKey: rateAppConfig.rateAppTileDisplayed)
+
+                controller.rateAppConfig = rateAppConfig
+                
+
+                expect(controller.nextAction) == .nothing
+            }
+
+            fit("should show rate app") {
+                let userDefaults: UserDefaults = UserDefaults(suiteName: "showRateApp")!
+                let controller: RateAppTileController = RateAppTileController(userDefaults: userDefaults)
+                
+                let rateAppConfig =  RateAppConfig()
+                rateAppConfig.numberOfLaunches = 20
+                rateAppConfig.daysAfterClickLike = 0
+
+                userDefaults.set(25, forKey: rateAppConfig.numberOfLaunchesDefaultsKey)
+                userDefaults.set(true, forKey: rateAppConfig.rateAppTileDisplayed)
+                userDefaults.set(true, forKey: rateAppConfig.isUserLikeAppKey)
+                userDefaults.set(true, forKey: rateAppConfig.clickedNoOnRateAppKey)
+                userDefaults.set(0, forKey: rateAppConfig.showedRateAppOnCountKey)
+                
+                controller.rateAppConfig = rateAppConfig
+                
+
+                expect(controller.nextAction) == .showRateApp
+            }
+            
+            it("should show rate app") {
+                let userDefaults: UserDefaults = UserDefaults(suiteName: "showRateAppNothing")!
+                let controller: RateAppTileController = RateAppTileController(userDefaults: userDefaults)
+                
+                let rateAppConfig =  RateAppConfig()
+                rateAppConfig.numberOfLaunches = 20
+                rateAppConfig.daysAfterClickLike = 10
+                
+                userDefaults.set(25, forKey: rateAppConfig.numberOfLaunchesDefaultsKey)
+                userDefaults.set(true, forKey: rateAppConfig.rateAppTileDisplayed)
+                userDefaults.set(true, forKey: rateAppConfig.isUserLikeAppKey)
+                userDefaults.set(true, forKey: rateAppConfig.clickedNoOnRateAppKey)
+                userDefaults.set(25, forKey: rateAppConfig.showedRateAppOnCountKey)
+
+                controller.rateAppConfig = rateAppConfig
+                
+
+                expect(controller.nextAction) == .nothing
+            }
+ 
+        }
+    }
+}
--- RateAppTileController.swift
+++ RateAppTileController.swift
@@ -1,8 +1,60 @@
-//
-//  RateAppTileController.swift
-//  RateAppTile
-//
-//  Created by Roman Barzyczak on 02/05/2020.
-//
-
 import Foundation
+
+public class RateAppTileController {
+    
+    private let userDefaults: UserDefaults
+    public var rateAppData: RateAppData?
+    public var rateAppConfig: RateAppConfig?
+
+    public var nextAction: RateAppAction {
+        return getNextAction()
+    }
+    
+    public var isDebug: Bool = false
+    
+    public init(userDefaults: UserDefaults) {
+        self.userDefaults = userDefaults
+    }
+    
+    
+    private var currentAppVersion: String {
+        // Get the current bundle version for the app
+        let infoDictionaryKey = kCFBundleVersionKey as String
+        guard let currentVersion = Bundle.main.object(forInfoDictionaryKey: infoDictionaryKey) as? String
+            else { fatalError("Expected to find a bundle version in the info dictionary") }
+        
+        return currentVersion
+    }
+    
+    private func getNextAction() -> RateAppAction {
+        guard let rateAppConfig = rateAppConfig else {
+            return .nothing
+        }
+        var count = userDefaults.integer(forKey: rateAppConfig.numberOfLaunchesDefaultsKey)
+        
+        if rateAppConfig.shouldCountLaunches {
+            count += 1
+            userDefaults.set(count, forKey: rateAppConfig.numberOfLaunchesDefaultsKey)
+        }
+        
+        let isRateAppDisplayed = userDefaults.bool(forKey: rateAppConfig.rateAppTileDisplayed)
+        let isUserLikeApp = userDefaults.bool(forKey: rateAppConfig.isUserLikeAppKey)
+        let clickedNoOnRateApp = userDefaults.bool(forKey: rateAppConfig.clickedNoOnRateAppKey)
+        let showedRateAppOnCount = userDefaults.integer(forKey: rateAppConfig.showedRateAppOnCountKey)
+
+        if isDebug || (!isRateAppDisplayed && count >= rateAppConfig.numberOfLaunches) {
+            userDefaults.set(true, forKey: rateAppConfig.rateAppTileDisplayed)
+            userDefaults.set(count, forKey: rateAppConfig.showedRateAppOnCountKey)
+
+            return .showRateTile
+        } else if (isUserLikeApp && clickedNoOnRateApp && count >= showedRateAppOnCount + rateAppConfig.daysAfterClickLike) {            userDefaults.set(currentAppVersion, forKey: rateAppConfig.lastVersionPromptedForReviewKey)
+            
+            return .showRateApp
+        }
+        return .nothing
+    }
+     
+    public enum RateAppAction {
+        case showRateApp, showRateTile, nothing
+    }
+}
